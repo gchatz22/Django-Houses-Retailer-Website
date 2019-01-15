95fb40a34e96a535a2d16df2c9f10b5f7a6a5dbe
Giannis <gchatz@mit.edu>
2019-01-15 14:51:26 -0500

    upload files

---
+++ houses_site/contacts/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect
+
+def contact(request):
+
+    return redirect('index')
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/giannischatziveroglou/Desktop/Django/Houses_Site/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
